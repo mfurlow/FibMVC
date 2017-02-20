299b9988a30fbbd8e23d079be439a79af5d744cf
michael <michael.a.furlow@gmail.com>
2017-02-20 10:38:17 -0500

    Created project

---
+++ FibMVC.Biz/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FibMVC.Biz
+{
+    public class Class1
+    {
+    }
+}
